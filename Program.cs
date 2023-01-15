6b24ff59c810c186bdbce2b86bfd544f1ee138a9
Alex <pirogav53@gmail.com>
2023-01-15 15:04:15 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+Console.Clear();
+int n = InputNumber("Введите число N: ");
+int m = InputNumber("Введите число M: ");
+System.Console.WriteLine(SumNumberRec(n,m));
+
+int SumNumberRec(int n, int m)
+{
+    if (n <= m)
+    {
+        return n + SumNumberRec(n + 1, m);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+//Проверка на число
+int InputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+
+    }
+    return number;
+}
